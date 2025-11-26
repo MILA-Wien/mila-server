32440565223256f1d4ab93b2303c40d3ce4c82e1
Joël <57440945+jofmi@users.noreply.github.com>
2025-11-26 17:21:32 +0100

    add lotzapp shop per iframe

---
+++ collectivo/app/pages/sortiment/aktuell.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth"],
+  layout: false,
+});
+
+const config = useRuntimeConfig();
+</script>
+
+<template>
+  <div class="iframe-container">
+    <iframe
+      :src="config.public.lotzappShopUrl"
+      title="MILA Sortiment Aktuell"
+      class="iframe"
+    />
+  </div>
+</template>
+
+<style scoped>
+.iframe-container {
+  width: 100%;
+  height: 100vh;
+}
+
+.iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
