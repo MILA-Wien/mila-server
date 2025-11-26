5db59a739460e3aeaf92c0327d111f9f31d3b6a9
Joël <57440945+jofmi@users.noreply.github.com>
2025-11-26 17:29:20 +0100

    add anny redirect

---
+++ collectivo/app/pages/raum.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Redirecting...</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth"],
+  layout: false,
+});
+
+const config = useRuntimeConfig();
+
+onMounted(() => {
+  if (config.public.annyLoginUrl) {
+    window.location.href = config.public.annyLoginUrl;
+  }
+});
+</script>
